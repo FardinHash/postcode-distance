841f72dc4bb3d050f18b6fc3c01b0ff2017e3515
FardinHash <fardin@iiec.iubat.edu>
2024-11-21 17:07:11 +0600

    Add health check route and fix port binding

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def find_postcodes():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 8080))  
+    app.run(debug=False, host='0.0.0.0', port=port)
